f78c5bbce72359b3f7c66835d81b083025af0368
Thevaashree02 <113322607+Thevaashree02@users.noreply.github.com>
2023-01-10 20:40:13 +0530

    Add files via upload

---
+++ FACTORIAL USING RECURSION.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int factorial(int n)
+{
+	if(n==1)
+	return 1;
+	else
+	return(n*factorial(n-1));
+}
+int main()
+{
+	int n;
+	printf("Enter the number:\n");
+	scanf("%d",&n);
+	printf(" The factorial of %d is = %d",n,factorial(n));
+}
